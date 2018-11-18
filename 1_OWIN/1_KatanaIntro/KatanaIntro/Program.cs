0864cf69c76e69e3ac5948e7f8477d96159230ac
nlivaic <nlivaic@gmail.com>
2018-11-18 11:20:57 +0100

    feat: How to create your own server with OWIN and Katana.
    Includes:
    1. Setting up a host console app.
    2. Create a server, provided by Katana components.
    3. Create middleware.
    4. Create an application (WebAPI).

---
+++ 1_OWIN/1_KatanaIntro/KatanaIntro/Program.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.Owin.Hosting;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace KatanaIntro
+{
+    using AppFunc = Func<IDictionary<string, object>, Task>;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string uri = "http://localhost:8080";
+            using (WebApp.Start<Startup>(uri))
+            {
+                Console.WriteLine("Started!");
+                Console.ReadKey();
+            }
+        }
+    }
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Dump out environment.
+            //app.Use(async (environment, next) =>
+            //{
+            //    foreach (var pair in environment.Environment)
+            //    {
+            //        Console.WriteLine($"{pair.Key}: {pair.Value}");
+            //    }
+            //    await next();
+            //});
+
+            // Dump out logging.
+            app.Use(async (environment, next) => 
+            {
+                Console.WriteLine($"Requesting: {environment.Request.Path}");
+                await next();
+                Console.WriteLine($"Response: {environment.Response.StatusCode}");
+            });
+
+            ConfigureWebApi(app);
+
+            // Dump out Hello World! to stream.
+            app.UseHelloWorld();
+        }
+
+        private void ConfigureWebApi(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            config.Routes.MapHttpRoute(
+                "DefaultRoute",
+                "api/{controller}/{id}",
+                new { id = RouteParameter.Optional });
+            app.UseWebApi(config);
+        }
+    }
+    
+    public static class AppBuilderExtensions
+    {
+        public static IAppBuilder UseHelloWorld(this IAppBuilder app)
+        {
+            return app.Use<HelloWorldComponent>();
+        }
+    }
+
+    public class HelloWorldComponent
+    {
+        AppFunc _next;
+        public HelloWorldComponent(AppFunc next)
+        {
+            _next = next;
+        }
+        public Task Invoke(IDictionary<string, object> environment)
+        {
+            var response = environment["owin.ResponseBody"] as Stream;
+            using (StreamWriter writer = new StreamWriter(response))
+            {
+                return writer.WriteAsync("Hello!!");
+            }
+        }
+    }
+}
